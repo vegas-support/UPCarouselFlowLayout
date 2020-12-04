2cfe3e14c7973527619e8d20fb3add5f94aa7cea
Rudy FREMONT <rudy.fremont@orange.com>
2020-12-04 11:36:38 +0100

    Add package

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "UPCarouselFlowLayout",
+    platforms: [ .iOS(.v9) ],
+    products: [
+        .library(
+            name: "UPCarouselFlowLayout", targets: ["UPCarouselFlowLayout"])
+        ],
+        dependencies: [ ],
+        targets: [
+            .target(
+                name: "UPCarouselFlowLayout",
+                path: "UPCarouselFlowLayout",
+                exclude: ["Info.plist", "UPCarouselFlowLayout.h"]),
+
+    ]
+)
